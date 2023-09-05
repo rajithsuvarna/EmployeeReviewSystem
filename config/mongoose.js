8d88a7c7428a3e0cfc4b19c0c63017098c9ec3f7
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2023-09-05 18:28:18 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ main().catch((err) => console.log(err));
 
 //connecting to mongodb
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/Employee_Review_System");
+  await mongoose.connect("mongodb+srv://rajithsuvarna49:kmVf31e7ExfbE4vS@cluster0.xffsluf.mongodb.net/?retryWrites=true&w=majority");
 }
 
 //to check the mongodb connection
